a1daddc332422f1e29b685968af9be1abbd05a40
Kristof Achten <kristofachten@gmail.com>
2021-12-04 00:11:35 +0100

    solution d3 part 2: still needs cleaning

--- src/d3_binary_diagnostic.rs
+++ src/d3_binary_diagnostic.rs
@@ -2,9 +2,69 @@ use crate::{get_input_for_day, split_on, to_int_32};
 
 pub fn run_diagnostic() {
     let binary_nums: Vec<String> = split_on(&get_input_for_day(3), "\n");
+    let values_vac = transpose(&binary_nums);
+
+    let mcv = to_most_common_value(&values_vac, binary_nums.len());
+
+    let lcv = to_least_common_value(&values_vac, binary_nums.len());
+
+    let gamma = isize::from_str_radix(&mcv.join(""), 2).unwrap();
+    let epsilon = isize::from_str_radix(&lcv.join(""), 2).unwrap();
+
+    let mut i = 0;
+
+    let mut oxygen_data = binary_nums.clone();
+    let mut co2_data = oxygen_data.clone();
+
+    while oxygen_data.len() > 1 {
+        let transposed_data = transpose(&oxygen_data);
+        let mcv = to_most_common_value(&transposed_data, oxygen_data.len());
+        let mcv_val = &mcv[i];
+        oxygen_data = oxygen_data.into_iter()
+            .filter(|v| &v.chars().nth(i).unwrap().to_string() == mcv_val)
+            .collect();
+        i += 1;
+    }
+
+    i = 0;
+    while co2_data.len() > 1 {
+        let transposed_data = transpose(&co2_data);
+        let mcv = to_least_common_value(&transposed_data, co2_data.len());
+        let mcv_val = &mcv[i];
+        co2_data = co2_data.into_iter()
+            .filter(|v| &v.chars().nth(i).unwrap().to_string() == mcv_val)
+            .collect();
+        i += 1;
+    }
+
+    let oxygen_binary = &mut oxygen_data[0];
+    let co2_binary = &mut co2_data[0];
+
+    oxygen_binary.remove(oxygen_binary.len() - 1);
+    co2_binary.remove(co2_binary.len() - 1);
+
+    let oxygen = isize::from_str_radix(&oxygen_binary, 2).unwrap();
+    let co2 = isize::from_str_radix(&co2_binary, 2).unwrap();
+
+    println!("part 1 = {} ; part 2 = {}", gamma * epsilon, oxygen * co2);
+}
+
+fn to_most_common_value(values_vac: &Vec<Vec<i32>>, size: usize) -> Vec<String> {
+    values_vac.iter()
+        .map(|v| most_common_value(v, size))
+        .collect::<Vec<String>>()
+}
+
+fn to_least_common_value(values_vac: &Vec<Vec<i32>>, size: usize) -> Vec<String> {
+    values_vac.iter()
+        .map(|v| least_common_value(v, size))
+        .collect::<Vec<String>>()
+}
+
+fn transpose(binary_nums: &Vec<String>) -> Vec<Vec<i32>> {
     let mut values_vac: Vec<Vec<_>> = Vec::new();
 
-    for num in &binary_nums {
+    for num in binary_nums {
         let vals: Vec<i32> = split_on(num.trim(), "").iter()
             .filter(|v| v != &"")
             .map(|v| to_int_32(&v))
@@ -21,24 +81,16 @@ pub fn run_diagnostic() {
             }
         }
     }
-
-    let gamma = isize::from_str_radix(&values_vac.iter()
-        .map(|v| to_most_common_value(v))
-        .collect::<Vec<String>>().join(""), 2).unwrap();
-    let epsilon = isize::from_str_radix(&values_vac.iter()
-        .map(|v| to_least_common_value(v))
-        .collect::<Vec<String>>().join(""), 2).unwrap();
-
-    println!("part 1 = {} ; part 2 = {}", gamma * epsilon, "Todo");
+    values_vac
 }
 
-fn to_most_common_value(vec: &Vec<i32>) -> String {
-    let more_zeros = vec.iter().filter(|v| v == &&to_int_32(&"0".to_string())).count() > 500;
+fn most_common_value(vec: &Vec<i32>, size: usize) -> String {
+    let more_zeros = vec.into_iter().filter(|v| v == &&to_int_32(&"0".to_string())).count() > size / 2;
     return if more_zeros { "0".to_string() } else { "1".to_string() }
 
 }
 
-fn to_least_common_value(vec: &Vec<i32>) -> String {
-    let more_zeros = vec.iter().filter(|v| v == &&to_int_32(&"0".to_string())).count() > 500;
+fn least_common_value(vec: &Vec<i32>, size: usize) -> String {
+    let more_zeros = vec.iter().filter(|v| v == &&to_int_32(&"0".to_string())).count() > size / 2;
     return if more_zeros { "1".to_string() } else { "0".to_string() }
 }
